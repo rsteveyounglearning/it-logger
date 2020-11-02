7f7ef8cdffae8c5cfa9859d9c4dc07468c77a569
R. Steve Young <r.steve.young.learning@gmail.com>
2020-11-02 00:51:01 -0300

    AddBtn, AddLogModal & EditLogModal Components.

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -32,7 +32,7 @@ const Logs = () => {
       {!loading && logs.length === 0 ? (
         <p className='center'>No logs to show...</p>
       ) : (
-        logs.map((log) => <LogItem log={log} key={log.id} />)
+        logs.map(log => <LogItem log={log} key={log.id} />)
       )}
     </ul>
   );
