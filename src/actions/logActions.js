4abc9a6c452763b2944b4172458fca99f5e35878
R. Steve Young <r.steve.young.learning@gmail.com>
2020-11-03 13:29:23 -0300

    Update Log

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -4,6 +4,9 @@ import {
 	LOGS_ERROR,
 	ADD_LOG,
 	DELETE_LOG,
+	UPDATE_LOG,
+	SET_CURRENT,
+	CLEAR_CURRENT,
 } from './types';
 
 // Get logs from server
@@ -71,6 +74,48 @@ export const deleteLog = id => async dispatch => {
 			payload: error.response.data,
 		});
 	}
+};
+
+// update log on server
+export const updateLog = log => async dispatch => {
+	try {
+		setLoading();
+
+		const res = await fetch(`/logs/${log.id}`, {
+			method: 'PUT',
+			body: JSON.stringify(log),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+
+		const data = await res.json();
+
+		dispatch({
+			type: UPDATE_LOG,
+			payload: data,
+		});
+	} catch (error) {
+		dispatch({
+			type: LOGS_ERROR,
+			payload: error.response.data,
+		});
+	}
+};
+
+// Set current log
+export const setCurrent = log => {
+	return {
+		type: SET_CURRENT,
+		payload: log,
+	};
+};
+
+// Clear current log
+export const clearCurrent = log => {
+	return {
+		type: CLEAR_CURRENT,
+	};
 };
 
 // Set loading to true
--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -1,91 +1,118 @@
-import React, { useState } from 'react';
-import M from 'materialize-css';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import M from 'materialize-css/dist/js/materialize.min.js';
+import { updateLog } from '../../actions/logActions';
 
-const EditLogModal = () => {
-  const [message, setMessage] = useState('');
-  const [attention, setAttention] = useState(false);
-  const [tech, setTech] = useState('');
+const EditLogModal = ({ current, updateLog }) => {
+	const [message, setMessage] = useState('');
+	const [attention, setAttention] = useState(false);
+	const [tech, setTech] = useState('');
 
-  const onSubmit = () => {
-    if (message === '' || tech === '') {
-      M.toast({ html: 'Please enter a message and tech' });
-    } else {
-      console.log(message, tech, attention);
+	useEffect(() => {
+		if (current) {
+			setMessage(current.message);
+			setAttention(current.attention);
+			setTech(current.tech);
+		}
+	}, [current]);
 
-      // Clear fields
-      setMessage('');
-      setTech('');
-      setAttention(false);
-    }
-  };
+	const onSubmit = () => {
+		if (message === '' || tech === '') {
+			M.toast({ html: 'Please enter a message and tech' });
+		} else {
+			const updLog = {
+				id: current.id,
+				message,
+				attention,
+				tech,
+				date: new Date(),
+			};
 
-  return (
-    <div id='edit-log-modal' className='modal' style={modalStyle}>
-      <div className='modal-content'>
-        <h4>Enter System Log</h4>
-        <div className='row'>
-          <div className='input-fields'>
-            <input
-              type='text'
-              name='message'
-              value={message}
-              onChange={e => setMessage(e.target.value)}
-            />
-            <label htmlFor='message' className='active'>
-              Log Message
-            </label>
-          </div>
-        </div>
-        <div className='row'>
-          <div className='input-field'>
-            <select
-              name='tech'
-              value={tech}
-              className='browser-default'
-              onChange={e => setTech(e.target.value)}
-            >
-              <option value='' disabled>
-                Select Technician
-              </option>
-              <option value='John Doe'>John Doe</option>
-              <option value='Sam Smith'>Sam Smith</option>
-              <option value='Sara Wilson'>Sara Wilson</option>
-            </select>
-          </div>
-        </div>
-        <div className='row'>
-          <div className='input-field'>
-            <p>
-              <label>
-                <input
-                  type='checkbox'
-                  className='filled-in'
-                  checked={attention}
-                  value={attention}
-                  onChange={e => setAttention(!attention)}
-                />
-                <span>Needs Attention</span>
-              </label>
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className='modal-footer'>
-        <a
-          href='#!'
-          onClick={onSubmit}
-          className='modal-close waves-effect blue waves-light btn'
-        >
-          Enter
-        </a>
-      </div>
-    </div>
-  );
+			updateLog(updLog);
+
+			M.toast({ html: `Log updated by ${tech}` });
+
+			// Clear fields
+			setMessage('');
+			setTech('');
+			setAttention(false);
+		}
+	};
+
+	return (
+		<div id='edit-log-modal' className='modal' style={modalStyle}>
+			<div className='modal-content'>
+				<h4>Enter System Log</h4>
+				<div className='row'>
+					<div className='input-fields'>
+						<input
+							type='text'
+							name='message'
+							value={message}
+							onChange={e => setMessage(e.target.value)}
+						/>
+					</div>
+				</div>
+				<div className='row'>
+					<div className='input-field'>
+						<select
+							name='tech'
+							value={tech}
+							className='browser-default'
+							onChange={e => setTech(e.target.value)}
+						>
+							<option value='' disabled>
+								Select Technician
+							</option>
+							<option value='John Doe'>John Doe</option>
+							<option value='Sam Smith'>Sam Smith</option>
+							<option value='Sara Wilson'>Sara Wilson</option>
+						</select>
+					</div>
+				</div>
+				<div className='row'>
+					<div className='input-field'>
+						<p>
+							<label>
+								<input
+									type='checkbox'
+									className='filled-in'
+									checked={attention}
+									value={attention}
+									onChange={e => setAttention(!attention)}
+								/>
+								<span>Needs Attention</span>
+							</label>
+						</p>
+					</div>
+				</div>
+			</div>
+			<div className='modal-footer'>
+				<a
+					href='#!'
+					onClick={onSubmit}
+					className='modal-close waves-effect blue waves-light btn'
+				>
+					Enter
+				</a>
+			</div>
+		</div>
+	);
+};
+
+EditLogModal.propTypes = {
+	current: PropTypes.object,
+	updateLog: PropTypes.func.isRequired,
 };
 
 const modalStyle = {
-  width: '75%',
-  height: '75%',
+	width: '75%',
+	height: '75%',
 };
 
-export default EditLogModal;
+const mapStateToProps = state => ({
+	current: state.log.current,
+});
+
+export default connect(mapStateToProps, { updateLog })(EditLogModal);
